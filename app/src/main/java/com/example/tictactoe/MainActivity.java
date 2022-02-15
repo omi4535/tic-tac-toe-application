7603b18a1190eb0f3d4b2d02699d017a0131813c
omkar dhage <omkardhage880500@gmail.com>
2022-02-15 15:24:47 +0530

    2

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import com.example.tictactoe.frangments.start_Fragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FrameLayout main_frame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+        main_frame = findViewById(R.id.main_fragment);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_fragment,new start_Fragment());
+        transaction.commit();
+
     }
 }
---
+++ app/src/main/java/com/example/tictactoe/frangments/start_Fragment.java
@@ -0,0 +1,22 @@
+package com.example.tictactoe.frangments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tictactoe.R;
+
+
+public class start_Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_start_, container, false);
+        return view;
+    }
+}
