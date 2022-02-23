289adeaca112a7e016f32f836fb3b509aa09ea25
omkar dhage <omkardhage880500@gmail.com>
2022-02-24 00:01:58 +0530

    game fragment created and play with frds is activated

--- app/src/main/java/com/example/tictactoe/frangments/start_Fragment.java
+++ app/src/main/java/com/example/tictactoe/frangments/start_Fragment.java
@@ -3,20 +3,31 @@ package com.example.tictactoe.frangments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.tictactoe.R;
 
 
 public class start_Fragment extends Fragment {
-
+private Button btn_with_frd,btn_with_comp;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_start_, container, false);
+        btn_with_frd = view.findViewById(R.id.with_frd);
+        btn_with_frd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.main_fragment,new game_Fragment());
+                transaction.commit();
+            }
+        });
         return view;
     }
 }
